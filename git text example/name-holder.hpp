459771839e5cc885c9417b08e79e813987f6f95e
Vladislav Tsibart <vitsibart@gmail.com>
2022-07-19 20:26:29 +0500

    fix

--- git text example/name-holder.hpp
+++ git text example/name-holder.hpp
@@ -2,13 +2,14 @@
 #define  NAME_HOLDER_HPP
 
 #include <string>
-#include <array>
+
+struct Array;
 
 
 void AddName(std::string name);
 
-template<typename type,size_t n>
-std::array<type,n> CurrentNames();
+
+const Array& CurrentNames();
 
 
 
