daf366c5aef608a7d2d4a7737c7402827e2bdf25
Vladislav Tsibart <vitsibart@gmail.com>
2022-07-19 23:16:15 +0500

    Добавлены описания

--- git text example/name-holder.hpp
+++ git text example/name-holder.hpp
@@ -3,15 +3,17 @@
 
 #include <string>
 
+
+//Простой массив. используется для передачи информации о хранимом списке имён
 struct Array {
 	const std::string *data;
 	int size;
 };
 
-
+//Добавляет имена в список имён
 void AddName(std::string name);
 
-
+//сообщает имена имеющиеся в списке в виде массива
 const Array& CurrentNames();
 
 
